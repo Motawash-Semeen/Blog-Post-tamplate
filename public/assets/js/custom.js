64587ce60dd2ed67ad64da84e8b7195fc2308a6b
MOtawash Semeen Chowdhury <motawash15-14166@diu.edu.bd>
2023-04-08 22:14:39 +0600

    hot topic

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -15,3 +15,36 @@ var swiper = new Swiper(".mySwiper", {
     },
 
 });
+
+
+
+$(document).ready(function() {
+    $('.topicSlider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
